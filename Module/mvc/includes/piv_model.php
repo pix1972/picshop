daec2c769185df9dc67228a501c6b62f8678fcef
Frank Klomfas <frank_klomfas@directbox.com>
2015-09-10 16:15:36 +0200

    Kleine Änderung und eine Vorlage

---
+++ Module/mvc/includes/piv_model.php
@@ -0,0 +1,15 @@
+<?php
+for ($i = 0; $i < 30; $i++) {
+
+	$image['img_' + $i] = array(
+		'name' 			=> 'Bild',
+		'filename'	=> 'img_' + $i,
+		'alt'				=> 'Erklärung',
+		'title'			=> 'site',
+		'endung'		=> '.jpeg',
+		'width'			=> 300,
+		'height'		=> 200
+		);
+};
+
+?>
---
+++ Module/testsite/objekt.js
@@ -0,0 +1,12 @@
+var link;
+
+link = {
+	start : {
+		'name' 	: 'wert',
+		'url'		: 'local'
+	},
+	wer : {
+		'name' 	: 'wert',
+		'url'		: 'local'
+	}
+};
